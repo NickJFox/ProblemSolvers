9560a43238c58328e76d7a541181fc131e9e54b9
Nick Fox <Nicholasfox4@gmail.com>
2023-09-28 17:22:50 -0500

    Display Post Function Started

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import './Card.css'
 function Card() {
     return (
         <>
-   <div className='Content'>CONTENT GETS POSTED HERE AND REMOVE THE GRAY AREA SO THIS GOES UP. MAKE THIS CODE INTO THE POST.JSX FEATURE ACTUALLY
+   <div className='Content'>
     </div>
     </>
     );
--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import './Post.css'
+import React, { useState } from 'react';
+import './Post.css';
 
 function CreatePost() {
-    return (
-        <>
-        <div className='CreatePost'>
-        <textarea />
-        <button className='PostButton'>Post</button>
-        </div>
+  const [enteredText, setEnteredText] = useState("");
+  const [submittedText, setSubmittedText] = useState(null);
+
+  const textChangeHandler = (event) => {
+    setEnteredText(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setSubmittedText(enteredText);
+    setEnteredText("");
+  };
+
+  return (
+    <>
+      <div className='CreatePost'>
+        <textarea
+          value={enteredText}
+          onChange={textChangeHandler}
+        />
+        <button className='PostButton' onClick={submitHandler}>Post</button>
+      </div>
+      {submittedText && <p>You just typed: {submittedText}</p>}
     </>
-    );
+  );
 }
 
 export default CreatePost;
